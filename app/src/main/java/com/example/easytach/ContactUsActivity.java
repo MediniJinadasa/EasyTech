72ea02a27616da4df42ef3d43bc89d4efe46be51
Medini Jinadasa <MediniJinadasa@users.noreply.github.com>
2020-08-10 23:55:11 +0530

    Update ContactUsActivity.java

--- app/src/main/java/com/example/easytach/ContactUsActivity.java
+++ app/src/main/java/com/example/easytach/ContactUsActivity.java
@@ -1,4 +1,15 @@
 package com.example.easytach;
 
-public class ContactUsActivity {
+import android.os.Bundle;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.TYPE_INPUT_METHOD_DIALOG);
+        setContentView(R.layout.activity_contact_us);
+    }
 }
